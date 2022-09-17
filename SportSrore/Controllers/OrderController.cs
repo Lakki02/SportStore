ece0eabc21bb20948390fa53ba6b72c24d1d901f
Lakki02 <lakki020896@gmail.com>
2022-09-17 16:29:01 +0300

    Admin panel - ship order

--- SportSrore/Controllers/OrderController.cs
+++ SportSrore/Controllers/OrderController.cs
@@ -17,6 +17,23 @@ namespace SportSrore.Controllers
             _cart = cartService;
         }
 
+        public ViewResult List() =>
+            View(_repository.Orders.Where(o => !o.Shipped));
+
+        [HttpPost]
+        public IActionResult MarkShipped(int orderID)
+        {
+            Order order = _repository.Orders
+                .FirstOrDefault(o => o.OrderId == orderID);
+            if (order != null)
+            {
+                order.Shipped = true;
+                _repository.SaveOrder(order);
+            }
+
+            return RedirectToAction(nameof(List));
+        }
+
         public ViewResult Checkout() => View(new Order());
 
         [HttpPost]
--- SportSrore/Migrations/20220917130352_added_ShippedOrders.Designer.cs
+++ SportSrore/Migrations/20220917130352_added_ShippedOrders.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportStore.Models;
 
 namespace SportSrore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220917130352_added_ShippedOrders")]
+    partial class added_ShippedOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
